8a27aa05037ba616db7bc83b716b39c285b7ee87
Kristof Feys <kristof.feys@me.com>
2016-10-06 13:41:08 +0200

    Create cask.sh

---
+++ cask.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Start
+echo "`basename $0` starting."
+
+# homebrew options
+export HOMEBREW_CASK_OPTS=""
+
+# Ask for the administrator password upfront
+sudo -v
+
+# setup taps
+brew tap caskroom/versions
+brew tap caskroom/fonts
+
+# install cask
+brew tap caskroom/cask
+
+# install applications
+brew cask install --require-sha ccleaner
+brew cask install --require-sha dash
+brew cask install --require-sha firefox
+brew cask install --require-sha flux
+brew cask install --require-sha sourcetree
+brew cask install --require-sha sublime-text
+brew cask install --require-sha the-unarchiver
+brew cask install --require-sha vagrant
+brew cask install --require-sha virtualbox
+brew cask install --require-sha virtualbox-extension-pack
+brew cask install --require-sha vlc
+
+# cleanup unneeded files
+brew cleanup
+
+# Finished
+echo "`basename $0` complete."
