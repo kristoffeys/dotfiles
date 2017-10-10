c09af7e53f52b528b66fe5342d51aa33926cdb04
Kristof Feys <kristof.feys@me.com>
2017-10-10 09:30:52 +0200

    Update composer.sh

--- composer.sh
+++ composer.sh
@@ -5,3 +5,4 @@ mv composer.phar /usr/local/bin/composer
 # Install global packages
 composer global require "banago/phploy"
 composer global require "laravel/valet"
+composer global require "phpmetrics/phpmetrics"
